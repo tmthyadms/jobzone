139034a1783b1b55b2bae513687892f37a0766cb
Timothy Adams J. <timothyadamsjenuil@gmail.com>
2023-10-28 23:20:34 +0800

    refactor: extract generic input into component

---
+++ components/app-input.vue
@@ -0,0 +1,42 @@
+<template>
+  <label class="block">
+    <span class="text-xs font-semibold opacity-60 capitalize">{{ label }}</span>
+    <slot>
+      <input
+        :type="type"
+        :id="inputId"
+        :name="inputId"
+        :placeholder="placeholder"
+        class="app-input"
+      />
+    </slot>
+  </label>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      default: 'text',
+    },
+    prompt: {
+      type: String,
+    },
+    inputId: {
+      type: String,
+    },
+  },
+  computed: {
+    placeholder() {
+      return this.prompt ? this.prompt : 'Enter your ' + this.label;
+    },
+  },
+};
+</script>
+
+<style scoped></style>
