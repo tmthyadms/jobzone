56baa585637ba4838eceb7e3ef4e1f407ade72b0
Timothy Adams J. <timothyadamsjenuil@gmail.com>
2023-10-28 15:41:32 +0800

    feat: add show/hide password

--- components/hero-get-started.vue
+++ components/hero-get-started.vue
@@ -41,16 +41,35 @@
                 <span class="text-xs font-semibold opacity-60 capitalize"
                   >password</span
                 >
-                <input
-                  type="password"
-                  id="password"
-                  placeholder="Enter your password"
-                  class="app-input"
-                />
+                <div class="relative">
+                  <input
+                    :type="passwordType"
+                    id="password"
+                    placeholder="Enter your password"
+                    class="app-input pr-5"
+                  />
+
+                  <div
+                    class="tooltip tooltip-left absolute end-0 top-1/2"
+                    style="translate: 0 -50%"
+                    :data-tip="passwordTip"
+                  >
+                    <label
+                      role="button"
+                      class="btn btn-xs btn-ghost btn-circle swap"
+                    >
+                      <input type="checkbox" v-model="password.reveal" />
+                      <IconEye class="swap-on" />
+                      <IconEyeSlash class="swap-off" />
+                    </label>
+                  </div>
+                </div>
               </label>
-              <button class="btn btn-block btn-primary">sign in</button>
+              <button type="submit" class="btn btn-block btn-primary">
+                sign in
+              </button>
             </form>
-            <button class="btn btn-outline btn-primary">
+            <button type="button" class="btn btn-outline btn-primary">
               new to jobzone? join now
             </button>
           </template>
@@ -59,3 +78,40 @@
     </template>
   </AppHero>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      password: {
+        show: {
+          type: 'text',
+          tip: 'Click to show password',
+        },
+        hide: {
+          type: 'password',
+          tip: 'Click to hide password',
+        },
+        reveal: false,
+      },
+    };
+  },
+  computed: {
+    passwordType() {
+      return this.password.reveal
+        ? this.password.show.type
+        : this.password.hide.type;
+    },
+    passwordTip() {
+      return this.password.reveal
+        ? this.password.hide.tip
+        : this.password.show.tip;
+    },
+    passwordIcon() {
+      return this.password.reveal
+        ? this.password.show.icon
+        : this.password.hide.icon;
+    },
+  },
+};
+</script>
