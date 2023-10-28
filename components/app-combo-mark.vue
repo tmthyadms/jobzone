f64f8b7e6264e63d291d7e3f0415a66e92097925
Timothy Adams J. <timothyadamsjenuil@gmail.com>
2023-10-28 23:17:22 +0800

    refactor: extract combo mark into component

---
+++ components/app-combo-mark.vue
@@ -0,0 +1,31 @@
+<template>
+  <component
+    :is="componentType"
+    id="app-combo-mark"
+    class="flex items-center gap-2"
+  >
+    <img src="images/logo/jz-32.png" alt="" class="app-img" />
+    <span href="#" class="font-bold text-xl normal-case"
+      >Job<span class="text-primary">zone</span></span
+    >
+  </component>
+</template>
+
+<script>
+export default {
+  props: {
+    link: {
+      type: Boolean,
+    },
+  },
+  computed: {
+    componentType() {
+      return this.link ? 'a' : 'div';
+    },
+  },
+  mounted() {
+    if (this.link)
+      document.getElementById('app-combo-mark').setAttribute('href', '#');
+  },
+};
+</script>
