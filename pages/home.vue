ae4ab33d2b41dcaac70b14a579725fc9269caca4
Timothy Adams J. <timothyadamsjenuil@gmail.com>
2023-11-03 17:59:56 +0800

    feat: add test home page

---
+++ pages/home.vue
@@ -0,0 +1,24 @@
+<template>
+  <AppHero>
+    <AppCard>
+      <template #body>
+        <h1>User _id: {{ id }}</h1>
+      </template>
+    </AppCard>
+  </AppHero>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      id: '',
+    };
+  },
+  async fetch() {
+    const data = await this.$axios.$post('/auths');
+    this.id = data[0]._id;
+    console.log(this.id);
+  },
+};
+</script>
