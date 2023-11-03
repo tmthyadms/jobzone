fe3c8a4c480655383d6d7d118c8684a2217f82a3
Timothy Adams J. <timothyadamsjenuil@gmail.com>
2023-11-03 17:15:56 +0800

    chore: remove redundant components

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,3 @@
 <template>
-  <div class="flex flex-col justify-between min-h-screen">
-    <TheHeader />
-    <main>
-      <HeroGetStarted />
-    </main>
-    <TheFooter />
-  </div>
+  <HeroSignIn />
 </template>
