7b7cb97cc669fd7a1addfb3ab628d9763b7af775
Timothy Adams J. <timothyadamsjenuil@gmail.com>
2023-11-03 17:12:40 +0800

    refactor: rename hero-get-started > hero-sign-in

---
+++ components/hero-sign-in.vue
@@ -0,0 +1,13 @@
+<template>
+  <AppHero
+    desc='Welcome to Jobzone, where your job search is safeguarded by cutting-edge machine learning, <br class="inline lg:hidden"/>ensuring a fraud-free experience.'
+    img="images/pass-job-form-6000x4000.jpg"
+    :center="false"
+  >
+    <template #side>
+      <div class="animated fadeInUp flex-1 w-full lg:w-96">
+        <CardSignIn />
+      </div>
+    </template>
+  </AppHero>
+</template>
