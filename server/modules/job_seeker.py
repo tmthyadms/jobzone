9f29a2851bcfdd8c47a826ceedc224ebdeee4bc5
Nik Zulfaqar <nikzulfaqar678@gmail.com>
2023-11-05 19:37:31 +0800

    Fix auth login logout issue and rename variables

--- server/modules/job_seeker.py
+++ server/modules/job_seeker.py
@@ -29,33 +29,33 @@ def update_job_seeker():
     data = request.get_json()
 
     job_seeker_id = data.get('jobSeekerId')
-    first_name = data.get('firstName')
-    last_name = data.get('lastName')
+    first_name = data.get('name').get('first')
+    last_name = data.get('name').get('last')
     gender = data.get('gender')
     country = data.get('country')
-    job_title = data.get('jobTitle')
-    company_name = data.get('companyName')
-    recent_exp = data.get('recentExp')
-    recent_edu = data.get('recentEdu')
+    job_title = data.get('recentExp').get('jobTitle')
+    company = data.get('recentExp').get('company')
+    level_edu = data.get('recentEdu').get('levelEdu')
+    field_study = data.get('recentEdu').get('fieldStudy')    
 
     job_seeker = {}
 
     if first_name:
-        job_seeker['firstName'] = first_name
+        job_seeker['name']['first'] = first_name
     if last_name:
-        job_seeker['lastName'] = last_name
+        job_seeker['lastName']['last'] = last_name
     if gender:
         job_seeker['gender'] = gender
     if country:
         job_seeker['country'] = country
     if job_title:
-        job_seeker['jobTitle'] = job_title
-    if company_name:
-        job_seeker['companyName'] = company_name
-    if recent_exp:
-        job_seeker['recentExperience'] = recent_exp
-    if recent_edu:
-        job_seeker['recentEducation'] = recent_edu
+        job_seeker['recentExperience']['jobTitle'] = job_title
+    if company:
+        job_seeker['recentExperience']['company'] = company
+    if level_edu:
+        job_seeker['recentEducation']['levelEducation'] = level_edu
+    if field_study:
+        job_seeker['recentEducation']['fieldStudy'] = field_study   
     
     mongo.db.jobseekers.update_one(
         {"_id": ObjectId(job_seeker_id)}, {"$set": job_seeker})
