249ab385ea6f86eb7467bb2fd4385600eeab3de9
Nik Zulfaqar <nikzulfaqar678@gmail.com>
2023-11-07 01:06:10 +0800

    Update reset update for job-seeker

--- server/modules/auth.py
+++ server/modules/auth.py
@@ -50,40 +50,55 @@ def create_auth():
     if accountType == 'job-seeker':
         job_seeker = {}
         job_seeker['_id'] = auth_id
-        job_seeker['name'] = {}
-        job_seeker['recentExperience'] = {}
-        job_seeker['recentEducation'] = {}
-        first_name = data.get('name').get('first')
-        last_name = data.get('name').get('last')
+
+        name = data.get('name') 
         gender = data.get('gender')
         country = data.get('country')
-        job_title = data.get('recentExp').get('jobTitle')
-        company = data.get('recentExp').get('company')
-        level_edu = data.get('recentEdu').get('levelEdu')
-        field_study = data.get('recentEdu').get('fieldStudy')       
+        recent_experience = data.get('recentExp') 
+        recent_education = data.get('recentEdu') 
+      
+        if name: 
+            job_seeker['name'] = {}
 
-        if first_name: job_seeker['name']['first'] = first_name
-        if last_name: job_seeker['name']['last'] = last_name
+            first_name = data.get('name').get('first')
+            last_name = data.get('name').get('last')
+
+            if first_name: job_seeker['name']['first'] = first_name
+            if last_name: job_seeker['name']['last'] = last_name
         if gender: job_seeker['gender'] = gender
         if country: job_seeker['country'] = country
-        if job_title: job_seeker['recentExperience']['jobTitle'] = job_title
-        if company: job_seeker['recentExperience']['company'] = company
-        if level_edu: job_seeker['recentEducation']['levelEducation'] = level_edu
-        if field_study: job_seeker['recentEducation']['fieldStudy'] = field_study     
+        if recent_experience:
+            job_seeker['recentExperience'] = {}
+
+            job_title = data.get('recentExp').get('jobTitle')
+            company = data.get('recentExp').get('company')
+
+            if job_title: job_seeker['recentExperience']['jobTitle'] = job_title
+            if company: job_seeker['recentExperience']['company'] = company
+        if recent_education: 
+            job_seeker['recentEducation'] = {}
+
+            level_edu = data.get('recentEdu').get('levelEdu')
+            field_study = data.get('recentEdu').get('fieldStudy') 
+
+            if level_edu: job_seeker['recentEducation']['levelEducation'] = level_edu
+            if field_study: job_seeker['recentEducation']['fieldStudy'] = field_study        
 
         mongo.db.jobseekers.insert_one(job_seeker).inserted_id
     elif accountType == 'business':
         business = {}
         business['_id'] = auth_id
-        company_name = data.get('compName')
-        registration_number = data.get('regNum')
+        business_name = data.get('bizName')
+        business_profile = data.get('bizProfile') #new field
+        registration_number = data.get('regNum') 
         address = data.get('address')
-        company_size = data.get('compSize')
+        business_size = data.get('bizSize')
         
-        if company_name: business['name'] = company_name
+        if business_name: business['businessName'] = business_name
+        if business_profile: business['businessProfile'] = business_profile
         if registration_number:business['registrationNumber'] = registration_number
         if address: business['address'] = address
-        if company_size: business['companySize'] = company_size
+        if business_size: business['businessSize'] = business_size
 
         mongo.db.businesses.insert_one(business).inserted_id
 
@@ -119,13 +134,13 @@ def update_auth():
     email = data.get('email')
     password = data.get('password')
 
-    auth_data = {}
+    auth = {}
 
     if auth_id:
-        if email: auth_data['email'] = email
-        if password: auth_data['password'] = password
+        if email: auth['email'] = email
+        if password: auth['password'] = password
 
-    mongo.db.auths.update_one({"_id": ObjectId(auth_id)}, {"$set": auth_data})
+    mongo.db.auths.update_one({"_id": ObjectId(auth_id)}, {"$set": auth})
     
     return "Auth updated."
 
