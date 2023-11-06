63e4cb2524901e8db8a84213c72c3bc18301880c
Timothy Adams J. <timothyadamsjenuil@gmail.com>
2023-11-07 02:26:09 +0800

    feat: add input textarea component

---
+++ components/input-textarea.vue
@@ -0,0 +1,49 @@
+<template>
+  <AppInput :label="label" :req="req">
+    <textarea
+      :id="newTextAreaId"
+      :name="newTextAreaId"
+      :placeholder="inputPlaceholder"
+      class="form-textarea app-field"
+      :required="req"
+      :autocomplete="ac"
+      @input="$emit('input', $event.target.value)"
+    ></textarea>
+  </AppInput>
+</template>
+
+<script>
+export default {
+  emits: ['change'],
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    prompt: {
+      type: String,
+    },
+    textAreaId: {
+      type: String,
+    },
+    new: {
+      type: Boolean,
+    },
+    req: {
+      type: Boolean,
+    },
+    ac: {
+      type: String,
+      default: 'off',
+    },
+  },
+  computed: {
+    newTextAreaId() {
+      return this.new ? `new-${this.textAreaId}` : this.selectId;
+    },
+    inputPlaceholder() {
+      return this.prompt ? this.prompt : 'Enter your ' + this.label;
+    },
+  },
+};
+</script>
