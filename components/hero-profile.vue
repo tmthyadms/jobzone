625380d27e210d2063ced18ed14707a31c562112
Timothy Adams J. <timothyadamsjenuil@gmail.com>
2023-11-10 00:32:32 +0800

    feat: implement profile page

---
+++ components/hero-profile.vue
@@ -0,0 +1,15 @@
+<template>
+  <AppHero :title="profile.fullName">
+    <div class="animated fadeInUp flex-1 w-full lg:w-96"></div>
+  </AppHero>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  computed: {
+    ...mapGetters('profile', ['profile']),
+  },
+};
+</script>
