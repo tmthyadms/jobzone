89a23cfa5649eaea477624c5de5cb0ed2d3209b8
Timothy Adams J. <timothyadamsjenuil@gmail.com>
2023-11-09 10:47:41 +0800

    feat: add subtitle + badges and new slot

--- components/app-card.vue
+++ components/app-card.vue
@@ -2,6 +2,7 @@
   <div
     class="card lg:card-side bg-base-100 text-base-content shadow"
     :class="{ 'image-full': overlay, hover: hover }"
+    @click="$emit('click', $event)"
   >
     <figure v-if="figure">
       <img :src="figure" :alt="title" class="app-img object-top" />
@@ -10,18 +11,38 @@
       <slot name="body">
         <h2 v-if="title" class="card-title capitalize">
           {{ title }}
-          <div v-if="badge" class="badge badge-secondary">{{ badge }}</div>
+          <div v-if="titleBadge" class="badge badge-secondary">{{ badge }}</div>
         </h2>
-        <p v-if="desc" class="text-xs opacity-60">{{ desc }}</p>
-        <div v-if="slot || badges" class="card-actions justify-end">
-          <slot></slot>
+        <h3
+          v-if="subtitle"
+          class="card-subtitle capitalize"
+          v-html="subtitle"
+        ></h3>
+        <div>
           <div
-            v-if="!slot && badges"
-            v-for="badge in badges"
-            class="badge badge-outline"
+            v-if="subtitleBadges"
+            v-for="subtitleBadge in subtitleBadges"
+            class="badge badge badge-primary capitalize"
           >
-            {{ badge }}
+            {{ subtitleBadge }}
           </div>
+        </div>
+        <p v-if="desc" class="text-xs opacity-60">{{ desc }}</p>
+        <slot></slot>
+        <div
+          v-if="!!$slots.action || badges"
+          class="card-actions"
+          :class="{ 'justify-end': badges }"
+        >
+          <slot name="action">
+            <div
+              v-if="badges"
+              v-for="badge in badges"
+              class="badge badge-outline"
+            >
+              {{ badge }}
+            </div>
+          </slot>
         </div>
       </slot>
     </div>
@@ -30,20 +51,22 @@
 
 <script>
 export default {
-  data() {
-    return {
-      slot: false,
-    };
-  },
+  emits: ['click'],
   props: {
     title: {
+      type: String,
+    },
+    subtitle: {
       type: String,
     },
     desc: {
       type: String,
     },
-    badge: {
+    titleBadge: {
       type: String,
+    },
+    subtitleBadges: {
+      type: Array,
     },
     badges: {
       type: Array,
@@ -58,13 +81,14 @@ export default {
       type: Boolean,
     },
   },
-  created() {
-    this.slot = !!this.$slots.default;
-  },
 };
 </script>
 
 <style scoped>
+.card-subtitle {
+  @apply text-sm;
+}
+
 .hover {
   @apply transition transition-transform duration-300 ease-out hover:-translate-y-1;
 }
