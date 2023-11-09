f6bbaf9eee427a1291b664ba40c661a9b4b4a4e0
Timothy Adams J. <timothyadamsjenuil@gmail.com>
2023-11-10 00:28:55 +0800

    feat: add responsiveness

--- components/app-combo-mark.vue
+++ components/app-combo-mark.vue
@@ -5,7 +5,7 @@
     class="flex items-center gap-2"
   >
     <img src="images/logo/jz-32.png" alt="" class="app-img" />
-    <span href="#" class="font-bold text-xl normal-case"
+    <span href="#" class="hidden lg:inline font-bold text-xl normal-case"
       >Job<span class="text-primary">zone</span></span
     >
   </component>
