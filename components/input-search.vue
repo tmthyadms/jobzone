bfb0fc1994fc83e4a457498830474dc592f24a69
Timothy Adams J. <timothyadamsjenuil@gmail.com>
2023-11-09 10:51:59 +0800

    feat: add input search component

---
+++ components/input-search.vue
@@ -0,0 +1,37 @@
+<template>
+  <AppInput>
+    <div class="relative">
+      <input
+        type="text"
+        :id="searchId"
+        :name="searchId"
+        :placeholder="prompt"
+        class="form-input app-field pr-5"
+        :disabled="disabled"
+        @input="$emit('input', $event.target.value)"
+      />
+      <IconSearch class="absolute end-0 top-1/2" style="translate: 0 -50%" />
+    </div>
+  </AppInput>
+</template>
+
+<script>
+export default {
+  emits: ['input'],
+  props: {
+    prompt: {
+      type: String,
+      default: 'Enter your search',
+    },
+    searchId: {
+      type: String,
+    },
+    new: {
+      type: Boolean,
+    },
+    disabled: {
+      type: Boolean,
+    },
+  },
+};
+</script>
