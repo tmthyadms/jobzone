d415744abc556225420a5dda22c1c0a89df0235f
Timothy Adams J. <timothyadamsjenuil@gmail.com>
2023-11-09 10:56:31 +0800

    chore: remove center attribute

--- components/hero-sign-up.vue
+++ components/hero-sign-up.vue
@@ -1,5 +1,5 @@
 <template>
-  <AppHero img="images/pass-job-form-6000x4000.jpg" :center="false">
+  <AppHero img="images/pass-job-form-6000x4000.jpg">
     <div class="animated fadeInUp flex-1 w-full lg:w-96">
       <CardSignUp />
     </div>
