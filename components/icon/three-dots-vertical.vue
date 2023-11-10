592d2b7247ab7f0918368eb7b02decdb2045a960
Timothy Adams J. <timothyadamsjenuil@gmail.com>
2023-11-10 12:15:29 +0800

    feat: add vertical menu icon

---
+++ components/icon/three-dots-vertical.vue
@@ -0,0 +1,37 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    :width="width"
+    :height="fallbackHeight"
+    :fill="fill"
+    class="bi bi-three-dots-vertical"
+    viewBox="0 0 16 16"
+  >
+    <path
+      d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    width: {
+      type: Number,
+      default: 16,
+    },
+    height: {
+      type: Number,
+    },
+    fill: {
+      type: String,
+      default: 'currentColor',
+    },
+  },
+  computed: {
+    fallbackHeight() {
+      return this.height || this.width;
+    },
+  },
+};
+</script>
