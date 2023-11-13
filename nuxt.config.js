14a15d1980168887ab581e71f643b94eb92c9b42
Timothy Adams J. <timothyadamsjenuil@gmail.com>
2023-11-13 20:32:38 +0800

    feat: add vue-toastification package

--- nuxt.config.js
+++ nuxt.config.js
@@ -38,12 +38,19 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+    'vue-toastification/nuxt',
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: 'http://localhost:6001',
+  },
+
+  toast: {
+    position: 'top-center',
+    timeout: 3000,
+    transition: 'Vue-Toastification__fade',
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
